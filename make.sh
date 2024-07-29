2b915e82a754f71f78feaebd2e7c7cf8add714dc
cvisotski <corey.visotski@wattswater.com>
2024-07-29 15:56:08 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,8 +3,8 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli build-docs ApiNexaTelemetry.openapi.yaml && \
-mv redoc-static.html index.html && \
+npx redoc-cli bundle ApiNexaTelemetry.openapi.yaml && \
+ren redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
 sed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' index.html && \
