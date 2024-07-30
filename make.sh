19d0a5b455bf155de93ff2f78240e20298b59f76
cvisotski <corey.visotski@wattswater.com>
2024-07-30 14:01:22 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx npx redocly build-docs docs/ApiNexaTelemetry.openapi.yaml && \
+npx redocly build-docs docs/ApiNexaTelemetry.openapi.yaml && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
